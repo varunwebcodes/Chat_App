eaaeb9fce0137011f04dade2cbdc7587607bce30
Varun Negi <varunnegiweb@gmail.com>
2026-03-16 14:08:01 +0530

    implement Socket.io

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const express = require('express');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express()
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/',(req,res)=>{
+    return res.sendFile('/public/index.html')
+})
+
+io.on('connection', (socket) => {
+  socket.on('user-message',(message)=>{
+    io.emit("message",message)
+  })
+});
+
+server.listen(3000, ()=>{
+    console.log("Server is running on port:3000")
+})
